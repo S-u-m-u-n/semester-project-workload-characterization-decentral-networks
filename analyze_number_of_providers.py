e1023df6f0fd99813e0fe9e3179c908d3bf52fab
jacobsi <jacobsi@student.ethz.ch>
2023-04-20 16:56:40 +0200

    make code more efficient

--- analyze_number_of_providers.py
+++ analyze_number_of_providers.py
@@ -28,7 +28,7 @@ def evaluate_data(language, sample_size):
     # pattern = r"^[a-z]{2}\.wikipedia-on-ipfs\.org_links_1_CID_providers_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.csv$"
     pattern = r"\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.csv$"
     for file in glob.glob(f"./data/{language}/{sample_size}_{measurement_name}/Providers/*.csv"):
-        print(file)
+        # print(file)
         if re.match(pattern, os.path.basename(file)):
             # print(file)
             df = pd.read_csv(file)
@@ -37,7 +37,7 @@ def evaluate_data(language, sample_size):
             reachable = len(df[df["Reachable"] == True])
             not_reachable = total - reachable
             data.append({"timestamp": timestamp, "total": total, "reachable": reachable, "not_reachable": not_reachable})
-            print(data)
+            # print(data)
 
     # Sort the data by timestamp
     data.sort(key=lambda x: x["timestamp"])
@@ -58,30 +58,24 @@ def parse_timestamp(filename):
     timestamp_str = re.search(r"\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.csv", basename).group()[:-4]
     return datetime.strptime(timestamp_str, "%Y-%m-%d_%H-%M-%S")
 
-# print(count_rows_in_csv('en' + str(wikipedia_file_suffix)))
-# print(int(count_rows_in_csv('en' + str(wikipedia_file_suffix)) * sample_percentage))
+languages = [('en', 'English', 'darkred'),
+            ('ru', 'Russian', 'darkblue'),
+            ('uk', 'Ukrainian', 'blue'),
+            ('tr', 'Turkish', 'red'),
+            ('ar', 'Arabic', 'yellow'),
+            ('zh', 'Chinese', 'black'),
+            ('my', 'Myanmar (Burmese)', 'teal'),
+            ('fa', 'Persian (Farsi)', 'orange')]
 
-en_timestamps, en_reachables = evaluate_data('en', int(count_rows_in_csv('en' + str(wikipedia_file_suffix)) * sample_percentage))
-ru_timestamps, ru_reachables = evaluate_data('ru', int(count_rows_in_csv('ru' + str(wikipedia_file_suffix)) * sample_percentage))
-uk_timestamps, uk_reachables = evaluate_data('uk', int(count_rows_in_csv('uk' + str(wikipedia_file_suffix)) * sample_percentage))
-tr_timestamps, tr_reachables = evaluate_data('tr', int(count_rows_in_csv('tr' + str(wikipedia_file_suffix)) * sample_percentage))
-ar_timestamps, ar_reachables = evaluate_data('ar', int(count_rows_in_csv('ar' + str(wikipedia_file_suffix)) * sample_percentage))
-zh_timestamps, zh_reachables = evaluate_data('zh', int(count_rows_in_csv('zh' + str(wikipedia_file_suffix)) * sample_percentage))
-my_timestamps, my_reachables = evaluate_data('my', int(count_rows_in_csv('my' + str(wikipedia_file_suffix)) * sample_percentage))
-fa_timestamps, fa_reachables = evaluate_data('fa', int(count_rows_in_csv('fa' + str(wikipedia_file_suffix)) * sample_percentage))
+# Evaluate data and store results in a dictionary
+data = {}
+for lang, _, _ in languages:
+    data[lang] = evaluate_data(lang, int(count_rows_in_csv(lang + str(wikipedia_file_suffix)) * sample_percentage))
 
 # Plot the data
 plt.figure(figsize=(10, 6))
-plt.plot(en_timestamps, en_reachables, label="English", color='darkred')
-plt.plot(ru_timestamps, ru_reachables, label="Russian", color='darkblue')
-plt.plot(uk_timestamps, uk_reachables, label="Ukrainian", color='blue')
-plt.plot(tr_timestamps, tr_reachables, label="Turkish", color='red')
-plt.plot(ar_timestamps, ar_reachables, label="Arabic", color='yellow')
-plt.plot(zh_timestamps, zh_reachables, label="Chinese", color='black')
-plt.plot(my_timestamps, my_reachables, label="Myanmar (Burmese)", color='teal')
-plt.plot(fa_timestamps, fa_reachables, label="Persian (Farsi)", color='orange')
-# plt.plot(timestamps, reachables, label="Reachable", color='green')
-# plt.plot(timestamps, not_reachables, label="Non-reachable", color='red')
+for lang, label, color in languages:
+    plt.plot(data[lang][0], data[lang][1], label=label, color=color)
 
 plt.xlabel("Timestamp")
 plt.ylabel("Number of unique providers")
